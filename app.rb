fcc39650e8f5025dafd3f463beeea092d5289298
ngottlieb87 <nogpdx@gmail.com>
2017-09-17 21:03:40 -0700

    non working post to server.

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require("sinatra")
-require("sinatra/loader")
+require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/task")
+require("./lib/list")
 require("pg")
 
 DB = PG.connect({:dbname=>"to_do"})
@@ -9,4 +10,17 @@ DB = PG.connect({:dbname=>"to_do"})
 get ('/') do
   @tasks = Task.all()
   @list = List.all()
+  erb(:input)
+end
+
+post ("/") do
+  list = params['name']
+  task = params['description']
+  new_list = List.new({:name=>list, :id => @id})
+  new_task = Task.new({:description=> task, :list_id => @list_id})
+  new_list.save()
+  new_task.save()
+  @tasks = Task.all()
+  @list = List.all()
+  erb(:to_do)
 end
